f15fcd934de7ffc0581d3717d91b49ac003a4c30
LanderDK <landerdekesel@gmail.com>
2025-10-23 11:13:07 +0200

    Add Comment entity with relationships to User and Article

---
+++ src/entity/Comment.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
+import { Article } from "./Article";
+
+@Entity()
+export class Comment {
+
+    @PrimaryGeneratedColumn()
+    CommentId: number;
+
+    @Column()
+    Content: string;
+
+    @Column()
+    PublishedAt: Date;
+
+    @ManyToOne(() => User, user => user.UserId, { cascade: true, eager: true })
+    UserId: number;
+
+    @ManyToOne(() => Article, article => article.ArticleId, { cascade: true, lazy: true })
+    ArticleId: number;
+}
