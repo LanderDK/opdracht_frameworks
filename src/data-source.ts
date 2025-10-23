ec530203e191b0711d0a1cd9fc0150fb6477d19b
LanderDK <landerdekesel@gmail.com>
2025-10-23 10:47:22 +0200

    Update database configuration to use environment variables and add dotenv dependency

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,17 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import { config } from "dotenv";
+
+config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     synchronize: true,
     logging: false,
     entities: [User],
