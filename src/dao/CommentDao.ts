e26fc0b3bb492fa9a224797a3cdd5508d995ecdf
LanderDK <landerdekesel@gmail.com>
2025-10-31 11:54:54 +0100

    refactor: remove unused import and implement CommentDAO for CRUD operations

---
+++ src/dao/CommentDao.ts
@@ -0,0 +1,42 @@
+import { Repository, FindManyOptions } from "typeorm";
+import { AppDataSource } from "../data/data-source";
+import { Comment } from "../data/entity/Comment";
+
+export class CommentDAO {
+  protected repo: Repository<Comment>;
+
+  constructor(protected ds = AppDataSource) {
+    this.repo = this.ds.getRepository(Comment);
+  }
+
+  async findAllByArticleId(
+    articleId: number,
+    options?: FindManyOptions<Comment>
+  ): Promise<Comment[]> {
+    return this.repo.find({
+      where: { ArticleId: articleId },
+      ...options,
+    });
+  }
+
+  async create(payload: Partial<Comment>): Promise<Comment> {
+    const comment = this.repo.create({
+      ...payload,
+      PublishedAt: new Date(),
+    });
+    return this.repo.save(comment);
+  }
+
+  async update(id: number, patch: Partial<Comment>): Promise<Comment | null> {
+    const comment = await this.repo.findOne({ where: { CommentId: id } });
+    if (!comment) return null;
+
+    Object.assign(comment, patch);
+    return this.repo.save(comment);
+  }
+
+  async delete(id: number): Promise<boolean> {
+    const result = await this.repo.delete({ CommentId: id });
+    return result.affected !== 0;
+  }
+}
--- src/data/entity/Comment.ts
+++ src/data/entity/Comment.ts
@@ -3,7 +3,6 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
-  OneToOne,
   PrimaryGeneratedColumn,
 } from "typeorm";
 import { Article } from "./Article";
