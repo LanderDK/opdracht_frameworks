b8865538f89659189747fe1e5e6118df3d77088d
Jason De Ridder <jason.deridder@ugent.be>
2025-10-31 20:21:44 +0100

    Added getarticlebyid route

--- src/routes/article.ts
+++ src/routes/article.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { ArticleDAO } from "../dao/ArticleDao";
+import Joi from "joi";
+import validate from "../core/validation";
 
 const articleDao = new ArticleDAO();
 
@@ -17,7 +19,32 @@ const getAllArticles = async (
   }
 };
 
+const getArticleById = async(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const articleId = parseInt(req.params.id, 10);
+    const article = await articleDao.findById(articleId);
+    if (!article) {
+      res.status(404).json({ message: "Article not found" });
+      return;
+    }
+    res.json(article);
+  } catch (error) {
+    next(error);
+  }
+};
+
+getArticleById.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive().required(),
+  },
+};
+
 export default function installArticleRouter(router: Router): void {
   // GET routes
   router.get("/articles", getAllArticles);
+  router.get("/articles/:id", validate(getArticleById.validationScheme), getArticleById);
 }
