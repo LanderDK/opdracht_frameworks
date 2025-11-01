f45b523b9ed1f72b52d1e328529d2e26ea80b489
LanderDK <landerdekesel@gmail.com>
2025-11-01 14:38:29 +0100

    Add onDelete cascade option to UserArticle relationships for improved data integrity

--- src/data/entity/UserArticle.ts
+++ src/data/entity/UserArticle.ts
@@ -13,14 +13,14 @@ export class UserArticle {
   @PrimaryColumn()
   ArticleId: number;
 
-  @ManyToOne(() => Article, { cascade: false })
+  @ManyToOne(() => Article, { cascade: false, onDelete: "CASCADE" })
   @JoinColumn({ name: "ArticleId" })
   Article: Article;
 
   @PrimaryColumn()
   UserId: number;
 
-  @ManyToOne(() => User, { cascade: false })
+  @ManyToOne(() => User, { cascade: false, onDelete: "CASCADE" })
   @JoinColumn({ name: "UserId" })
   User: User;
 }
