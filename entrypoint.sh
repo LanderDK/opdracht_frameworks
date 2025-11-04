1be13174f25851e2f6d818985594c05b30cf20f5
Jason De Ridder <jason.deridder@ugent.be>
2025-11-04 20:11:13 +0100

    implemented 2 start methods: dev local, server for docker.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 set -e
 
-# Wait for MySQL to be ready
-echo "Waiting for MySQL..."
-until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT
-do
-  echo "Waiting for database connection..."
-  sleep 2
-done
-
 # Seed the database
 echo "Seeding database..."
-npm run seed
+npm run seed_docker
 
 # Start the app
 echo "Starting app..."
